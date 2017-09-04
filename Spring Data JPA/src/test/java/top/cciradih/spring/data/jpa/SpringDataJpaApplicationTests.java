f3a36948d3e52b9764babb74f5ec904a5d23a094
cciradih <hidarichaochen@gmail.com>
2017-09-04 09:08:29 +0800

    init

--- Spring Data JPA/src/test/java/top/cciradih/spring/data/jpa/SpringDataJpaApplicationTests.java
+++ Spring Data JPA/src/test/java/top/cciradih/spring/data/jpa/SpringDataJpaApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DatajpaApplicationTests {
+public class SpringDataJpaApplicationTests {
     @Autowired
     private UserRepository repository;
 
