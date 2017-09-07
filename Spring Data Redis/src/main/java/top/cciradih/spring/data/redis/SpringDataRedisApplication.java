738606166f7f03569d734acc0e5f435883fb5467
cciradih <hidarichaochen@gmail.com>
2017-09-07 14:00:40 +0800

    init

--- Spring Data Redis/src/main/java/top/cciradih/spring/data/redis/SpringDataRedisApplication.java
+++ Spring Data Redis/src/main/java/top/cciradih/spring/data/redis/SpringDataRedisApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataredisApplication {
+public class SpringDataRedisApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataredisApplication.class, args);
+		SpringApplication.run(SpringDataRedisApplication.class, args);
 	}
 }
